299dc6442e765c9fb0191dd59c3b7123da421226
luca-conterio <luca.cont96@gmail.com>
2019-06-29 20:38:38 +0200

    update

--- second_project/src/node.cpp
+++ second_project/src/node.cpp
@@ -6,12 +6,33 @@
 
 using namespace ros;
 
+/* void computeOdometry(double, double);
+void publishOdometr();
+void subscriberCallback(const geometry_msgs::PointStamped&); */
+
 // car baseline: distance between the wheels
 static const double baseline = 1.3;
 // distance between front and rear wheels of the car
 static const double front_rear_wheels_distance = 1.765;
 // steering ratio: for each 18° of the steer, the wheels rotate by 1°
 static const double steering_ratio = 18;
+
+/*
+class PubSub {
+    private:
+        static ros::Publisher pub;
+        ros::Subscriber sub;
+    
+    public: PubSub(ros::NodeHandle n) {
+        pub = n.advertise<nav_msgs::Odometry>("/car_odometry", 50);
+        sub = n.subscribe("/speedsteer", 1000, &subscriberCallback);
+    }
+
+    public: static void publish(nav_msgs::Odometry odom_msg) {
+        pub.publish(odom_msg);
+    }
+};
+*/
 
 // contains data computed via Ackerman odometry
 typedef struct od_data {
@@ -31,7 +52,9 @@ Time current_time;
 Time last_time;
 
 // computes the Ackerman odometry, given the steering angle and the velocity of the car
-void computeOdometry(double steering, double velocity) {
+void computeOdometry(const geometry_msgs::PointStamped& msg) {
+    double steering = msg.point.x;
+    double velocity = msg.point.y;
     current_time = Time::now();
     double wheels_angle = (steering / steering_ratio) * M_PI / 180; // convert to radians
     double dt = (current_time - last_time).toSec();
@@ -45,9 +68,18 @@ void computeOdometry(double steering, double velocity) {
 }
 
 // publish odometry data on the two topics: /car_odometry and /car_odometry_with_type
-void publishOdometry(ros::Publisher pub) {
+void publishOdometry(ros::Publisher pub, tf::TransformBroadcaster odom_broadcaster) {
     nav_msgs::Odometry odom;
     geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(odometry_data.theta);
+
+    // publish the tf transform
+    tf::Transform transform;
+    transform.setOrigin(tf::Vector3(odometry_data.x, odometry_data.y, 0.0));
+    tf::Quaternion q;
+    q.setRPY(0, 0, odometry_data.theta);
+    transform.setRotation(q);
+    odom_broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom", "car_frame"));
+
     // fill the odometry message with data
     odom.header.stamp = odometry_data.current_time;
     odom.header.frame_id = "odom";
@@ -65,31 +97,26 @@ void publishOdometry(ros::Publisher pub) {
     pub.publish(odom);
 }
 
-void subscriberCallback(const geometry_msgs::PointStamped& msg) {
-    //ROS_INFO("Received: %lf - %lf", msg.point.x, msg.point.y);
-    computeOdometry(msg.point.x, msg.point.y);
-    //ROS_INFO("Publishing");
-    //publishOdometry(pub);
-}
-
 int main(int argc, char **argv) {
 
     ros::init(argc, argv, "odometry_node");
 
     ros::NodeHandle n;
-
-    ros::Publisher odometry_pub = n.advertise<nav_msgs::Odometry>("/car_odometry", 50);
-    ros::Subscriber sub = n.subscribe("/speedsteer", 1000, subscriberCallback);
-
-    last_time = Time::now();
+    ros::Publisher pub = n.advertise<nav_msgs::Odometry>("/car_odometry", 50);
+    ros::Subscriber sub = n.subscribe("/speedsteer", 1000, computeOdometry);
+    tf::TransformBroadcaster odom_broadcaster;
+    //PubSub *pubsub = new PubSub(n);
 
     ROS_INFO("Starting odometry node");
 
-    ros::Rate rate(30.0);
-
+    last_time = Time::now();
+    
     while(ros::ok()) {
         ros::spinOnce();
-        publishOdometry(odometry_pub);
+        publishOdometry(pub, odom_broadcaster);
     }
+    
     return 0;
 }
+
+
